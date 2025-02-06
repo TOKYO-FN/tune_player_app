e2f0a208e7dcd352361abd9c3c997e66b26e7a9e
TOKYO-FN <youssef.magdyii.12@gmail.com>
2025-02-06 04:47:39 +0200

    `Refactor TuneComponent to use StatefulWidget and AudioPlayer`

--- lib/widgets/tune_component.dart
+++ lib/widgets/tune_component.dart
@@ -2,7 +2,7 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:tune_player_app/models/tune_model.dart';
 
-class Tune extends StatelessWidget {
+class Tune extends StatefulWidget {
   const Tune({
     super.key,
     required this.tuneModel,
@@ -11,18 +11,41 @@ class Tune extends StatelessWidget {
   final TuneModel tuneModel;
 
   @override
+  _TuneState createState() => _TuneState();
+}
+
+class _TuneState extends State<Tune> {
+  late AudioPlayer _player;
+
+  @override
+  void initState() {
+    super.initState();
+    _player = AudioPlayer();
+    _preloadSounds();
+  }
+
+  void _preloadSounds() {
+    _player.setSource(AssetSource(widget.tuneModel.sound));
+  }
+
+  void _playSound(String sound) async {
+    if (_player.state == PlayerState.playing) {
+      await _player.stop();
+    }
+
+    await _player.play(AssetSource(sound));
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Expanded(
-      flex: tuneModel.flex,
+      flex: widget.tuneModel.flex,
       child: GestureDetector(
         onTap: () {
-          final player = AudioPlayer();
-          player.play(
-            AssetSource(tuneModel.sound),
-          );
+          _playSound(widget.tuneModel.sound);
         },
         child: Container(
-          color: tuneModel.color,
+          color: widget.tuneModel.color,
           width: double.infinity,
         ),
       ),
